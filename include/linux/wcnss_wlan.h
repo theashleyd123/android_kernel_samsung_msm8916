773fea91903b5689398c23977945bc693a009e86
Anand N Sunkad <asunka@codeaurora.org>
2015-04-30 21:27:06 +0530

    wcnss: Add API to send IRIS name
    As per requirment, user need to check WLAN RF card
    preset on device at run time. WLAN host driver use
    this API to get the IRIS card name.
    
    CRs-Fixed: 831585
    Change-Id: I8bd71cab7f998cde01bd43068b8dfbfc5817fb7f
    Signed-off-by: Anand N Sunkad <asunka@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -32,6 +32,7 @@ struct wcnss_wlan_config {
 	int	is_pronto_vt;
 	int	is_pronto_v3;
 	void __iomem	*msm_wcnss_base;
+	int	iris_id;
 	int	vbatt;
 };
 
@@ -123,6 +124,7 @@ int wcnss_wlan_iris_xo_mode(void);
 void wcnss_flush_work(struct work_struct *work);
 void wcnss_flush_delayed_work(struct delayed_work *dwork);
 void wcnss_dump_stack(struct task_struct *task);
+int wcnss_get_iris_name(char *iris_version);
 
 #ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 void wcnss_log_debug_regs_on_bite(void);
